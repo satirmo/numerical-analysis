0c95f54d1472c961113c314b059180e5b7d4ae96
Tomás <satirmo@gmail.com>
2022-01-18 22:06:40 -0800

    Update forward_euler.m
    Fix size of y points matrix

--- forward_euler.m
+++ forward_euler.m
@@ -1,6 +1,6 @@
 function [y] = forward_euler(f, t_init, y_init, t_max, h)
     timesteps = t_init : h : t_max;
-    y = zeros(numel(timesteps), 2);
+    y = zeros(numel(timesteps) + 1, 2);
     y(1,:) = y_init;
 
     for k = 1 : numel(timesteps)
