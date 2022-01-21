e3f475663ec3968b8fc474464b4f2147bd6e789b
Tomás <satirmo@gmail.com>
2022-01-21 01:25:27 -0800

    Update calculate_stability.m

--- calculate_stability.m
+++ calculate_stability.m
@@ -1,40 +1,19 @@
-function [result] = calculate_stability(system_matrix)
+function [result] = calculate_stability(system_matrix, eps)
+    if nargin == 1
+        eps = 1e-8;
+    end
+    
     eigenvals = eig(system_matrix);
-    result = "";
+    result = "unstable";
 
-    if is_unstable(eigenvals)
-        result = "unstable";
-    elseif is_asymptotically_stable(eigenvals)
+    if norm(eigenvals) < eps
+        eigenvals = zeros(size(eigenvals));
+    end
+
+    if is_asymptotically_stable(eigenvals)
         result = "asymptotically stable";
-    elseif is_stable(eigenvals)
+    elseif is_stable(system_matrix, eigenvals)
         result = "stable";
-    end
-end
-
-function [result] = is_unstable(eigenvals)
-    real_parts = real(eigenvals);
-    result = false;
-
-    for k = 1 : numel(real_parts)
-        if real_parts(k) > 0
-            result = true;
-        end
-    end
-end
-
-function [result] = is_stable(eigenvals)
-    real_parts = real(eigenvals);
-    unique_eigenvals = unique(eigenvals);
-    result = true;
-
-    for k = 1 : numel(real_parts)
-        if real_parts(k) > 0
-            result = false;
-        end
-
-        if and(real_parts(k) == 0, not(any(unique_eigenvals == eigenvals(k))))
-            result = false;
-        end
     end
 end
 
@@ -45,6 +24,31 @@ function [result] = is_asymptotically_stable(eigenvals)
     for k = 1 : numel(real_parts)
         if real_parts(k) >= 0
             result = false;
+            break;
+        end
+    end
+end
+
+function [result] = is_stable(system_matrix, eigenvals)
+    zero_matrix = zeros(size(system_matrix));
+    identity_matrix = eye(size(system_matrix));
+    
+    real_parts = real(eigenvals);
+    result = true;
+
+    for k = 1 : numel(real_parts)
+        if real_parts(k) > 0
+            result = false;
+            break;
+        end
+
+        algebraic_multiplicity = length(find(eigenvals == eigenvals(k)));
+
+        if and(real_parts(k) == 0, algebraic_multiplicity > 1)
+            if system_matrix - eigenvals(k)*identity_matrix ~= zero_matrix
+                result = false;
+                break;
+            end
         end
     end
 end
